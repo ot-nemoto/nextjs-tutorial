821b65d80c7b3d8e50a6a99e5b67f4bd999c8653
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2024-04-20 08:49:17 +0000

    Clerk の準備
    ref https://zenn.dev/nino/books/30e21d37af73b5/viewer/init-clerk

--- README.md
+++ README.md
@@ -3,3 +3,26 @@
 ## [Next.js 14 アプリ開発のあらすじ](https://zenn.dev/nino/books/30e21d37af73b5)
 
 -   [my-app](./my-app)
+
+```sh
+bun run debug
+```
+
+-   http://localhost:3000/
+
+### Configuration
+
+_Clerk_
+
+```sh
+cat <<EOT | tee .env.local
+NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=your_publishable_key
+CLERK_SECRET_KEY=your_secret_key
+EOT
+```
+
+### 関連サービス
+
+-   [Clerk](https://clerk.com/)
+-   [Cloudflare](https://www.cloudflare.com/)
+-   [Vercel](https://vercel.com/)
---
+++ my-app/middleware.ts
@@ -0,0 +1,22 @@
+// ref https://clerk.com/docs/references/nextjs/auth-middleware
+// import { authMiddleware } from "@clerk/nextjs";
+// ref https://clerk.com/docs/references/nextjs/clerk-middleware
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+// export default authMiddleware({
+//     // 公開ルートを指定（利用規約やログイン画面などログイン不要でアクセスできるルート）
+//     publicRoutes: ['/', '/terms'],
+// });
+
+const isProtectedRoute = createRouteMatcher(['/(.*)', '/terms(.*)']);
+
+export default clerkMiddleware((auth, req) => {
+    console.log('xxxxxxxxxxxxxxxxxxxxxxxxx');
+    console.log(req);
+    console.log(isProtectedRoute(req));
+    if (!isProtectedRoute(req)) auth().protect();
+});
+
+export const config = {
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
