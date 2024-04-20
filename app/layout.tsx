41063763f2c075acff81cc3fc03af088f212c7a5
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2024-04-20 00:40:16 +0000

    Pages and Layouts
    ref https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,10 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                {/* Layout UI */}
+                <main>{children}</main>
+            </body>
         </html>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Next.js',
+};
+
 // `app/page.tsx` is the UI for the `/` URL
 export default function Page() {
     return <h1>Hello, Home page!</h1>;
