150e25e226d4bf443a6c5196aa8cbcd1059a2ce9
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2024-05-03 06:26:54 +0000

    modify app.config.ts
    - アプリケーションのサマリ情報を修正

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,5 @@
 export const AppConfig = {
-  title: 'Next.js 14 Demo',
-  description: 'Next.js 14 のデモアプリ',
-  githubURL: 'https://github.com/dninomiya/next-demo-app/tree/main',
+  title: 'nextjs workshop',
+  description: 'nextjs workshop',
+  githubURL: 'https://github.com/ot-nemoto/nextjs-workshop',
 };
