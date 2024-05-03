84e984b8f14a61c96a3fbfd49659936fb1cead4b
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2024-05-03 06:43:10 +0000

    ヘッダーのリンク先およびアバター画像の変更

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,3 +1,4 @@
+import { AppConfig } from '@/app.config';
 import Image from 'next/image';
 import avatar from './avatar.png';
 
@@ -5,7 +6,7 @@ export default function Footer() {
   return (
     <footer className="text-center mt-4 text-gray-500 py-4 px-6">
       <a
-        href="https://twitter.com/d151005"
+        href={AppConfig.githubURL}
         target="_blank"
         className="inline-flex gap-2 items-center justify-center"
       >
